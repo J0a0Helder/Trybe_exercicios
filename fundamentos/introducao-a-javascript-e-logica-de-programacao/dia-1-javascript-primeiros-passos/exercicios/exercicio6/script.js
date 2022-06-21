4b736591ea09002bba3302392cef05fae795e629
João H <joaoheldermartins@hotmail.com>
2022-06-21 18:35:02 -0300

    resolução do exercicio 6

---
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/exercicio6/script.js
@@ -0,0 +1,22 @@
+let peca = "RainHa".toLowerCase()
+
+switch (peca) {
+    case "rei":
+        console.log("vertical, horizontal ou diagonal")
+        break;
+    case "rainha":
+        console.log("colunas, fileiras e diagonal")
+        break;
+    case "cavalo":
+        console.log("duas casas para frente e uma para a esquerda ou direita")
+        break;
+    case "torre":
+        console.log("colunas ou fileiras")
+        break;
+    case "peao":
+        console.log("diagonal")
+        break;
+    default:
+        console.log("peça inválida")
+        break;
+}
