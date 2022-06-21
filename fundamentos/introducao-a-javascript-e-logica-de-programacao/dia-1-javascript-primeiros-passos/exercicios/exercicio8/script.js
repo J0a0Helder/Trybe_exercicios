d97dc2b6827ad5bd3f8b9a7b1863a7b8b0f6b330
João H <joaoheldermartins@hotmail.com>
2022-06-21 18:59:24 -0300

    resolução do exercicio 9

--- fundamentos/introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/exercicio8/script.js
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/exercicio8/script.js
@@ -1,6 +1,6 @@
 const a = 3;
-const b = 5;
-const c = 10;
+const b = 2;
+const c = 11;
 
 if(a%2 === 0 || b%2 === 0 || c%2 === 0){
     console.log("true");
---
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/exercicio9/script.js
@@ -0,0 +1,10 @@
+const a = 22;
+const b = 44;
+const c = 158;
+
+if(a%2 === 1 || b%2 === 1 || c%2 === 1){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
