21dea967e93038c9ec1baffcff1c3572ff61aea8
João H <joaoheldermartins@hotmail.com>
2022-06-27 14:14:30 -0300

    resolução exercicio 4

--- fundamentos/introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/Funcoes/exercicio4.js
+++ fundamentos/introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/Funcoes/exercicio4.js
@@ -1,2 +1,13 @@
-//Crie uma função que receba um array de nomes e retorne o nome com a menor quantidade de caracteres.
+//Crie uma função que receba um array de nomes e retorne o nome com a menor quantidade de caracteres
 
+function maiorNome(palavras) {
+    let maior = palavras[0];
+    for(let index in palavras){
+        if(maior.length < palavras[index].length){
+            maior = palavras[index];
+        }
+    }
+    return maior;
+}
+
+console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])); 
