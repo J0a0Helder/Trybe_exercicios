8ede0cd3ffd968a7eb02e9bcdbca3190332adf49
João H <joaoheldermartins@hotmail.com>
2022-06-28 18:52:53 -0300

    resolução do exercicio 4

--- fundamentos/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,19 +1,23 @@
-function trocarParagrafo() {
+function textoH1() {
     let paragrafo = document.getElementsByTagName('p')[1];
     paragrafo.innerHTML = "Estar em um bom emprego e estar desenvolvendo softwares de qualidade!"
   }
-  trocarParagrafo();
+  textoH1();
 
-  function trocaAmarelo(){
+  function trocaAmarelo() {
     let content = document.getElementsByClassName('main-content')[0];
     content.style.background = 'rgb(76,164,109)';
   }
   trocaAmarelo();
 
-  function TrocarVermelho(){
+  function TrocarVermelho() {
     let centerContent = document.getElementsByClassName('center-content')[0];
     centerContent.style.background = '#fff';
   }
   TrocarVermelho();
 
-  
+  function textoH1() {
+    let corretor = document.getElementsByTagName('h1')[0];
+    corretor.innerText = "Exercício 5.1 - JavaScript"
+  }
+  textoH1();
