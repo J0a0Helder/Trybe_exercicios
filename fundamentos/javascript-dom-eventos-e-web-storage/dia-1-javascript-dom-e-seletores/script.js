e25ea749ab96598c125d2534b0444abcff7b79b5
João H <joaoheldermartins@hotmail.com>
2022-06-28 18:50:17 -0300

    resolução do exercicio 3

--- fundamentos/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -2,10 +2,18 @@ function trocarParagrafo() {
     let paragrafo = document.getElementsByTagName('p')[1];
     paragrafo.innerHTML = "Estar em um bom emprego e estar desenvolvendo softwares de qualidade!"
   }
-  trocarParagrafo()
+  trocarParagrafo();
 
-  function trocarTexto(){
+  function trocaAmarelo(){
     let content = document.getElementsByClassName('main-content')[0];
     content.style.background = 'rgb(76,164,109)';
   }
-  trocarTexto()
+  trocaAmarelo();
+
+  function TrocarVermelho(){
+    let centerContent = document.getElementsByClassName('center-content')[0];
+    centerContent.style.background = '#fff';
+  }
+  TrocarVermelho();
+
+  
