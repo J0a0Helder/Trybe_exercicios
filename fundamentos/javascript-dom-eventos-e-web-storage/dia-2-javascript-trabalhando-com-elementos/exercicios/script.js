b42d0233ac78970578778a8fcd9dc489ea1afb02
João H <joaoheldermartins@hotmail.com>
2022-06-29 18:34:50 -0300

    resolução do exercicio 1, 2 e 3

---
+++ fundamentos/javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -0,0 +1,15 @@
+// 1 
+const h1 = document.createElement('h1');
+h1.innerText = 'Exercício 5.2 - JavaScript DOM';
+document.body.appendChild(h1);
+
+// 2 
+const main = document.createElement('main');
+main.className = ('main-content');
+document.body.appendChild(main);
+
+// 3 
+const section = document.createElement('section');
+section.className = ('center-content');
+main.appendChild(section);
+
