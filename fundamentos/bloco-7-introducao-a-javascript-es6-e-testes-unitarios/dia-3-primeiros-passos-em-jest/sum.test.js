0c3d144844ea9821b52617291a8d184a3d46bbf9
João H <joaoheldermartins@hotmail.com>
2022-07-18 19:16:25 -0300

    exercicio 1.3 e 1.4

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -8,4 +8,11 @@ describe('A função retorna com sucesso: ', () => {
   test('0 quando sum recebe 0 e 0', () => {
     expect(sum(0, 0)).toEqual(0);
   });
+
+  test('Erro quando sum recebe 4 e "5"', () => {
+    expect(() => { sum() }).toThrow();
+  });
+
+  test('A mensagem de erro "parameters must be numbers" quando não recebe um parametro válido', () => {
+    expect(() => { sum() }).toThrowError(new Error('parameters must be numbers'))});
 });
