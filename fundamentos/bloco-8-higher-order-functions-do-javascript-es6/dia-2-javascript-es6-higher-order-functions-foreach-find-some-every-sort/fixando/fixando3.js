a7cfc804f2b82cd597940c84a758433b5e3b3fc0
João H <joaoheldermartins@hotmail.com>
2022-07-22 18:37:46 -0300

    resolução do fixando 3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixando/fixando3.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixando/fixando3.js
@@ -1,3 +1,4 @@
+// fixando 3.1
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
 const hasName = (arr, name) => {
@@ -7,3 +8,20 @@ const hasName = (arr, name) => {
 console.log(hasName(names, 'José'));
 console.log(hasName(names, 'Ana'));
 console.log(hasName(names, 'Julio'));
+
+// fixando 3.2
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((all) => all.age >= minimumAge);
+}
+
+console.log(verifyAges(people, 15));
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 16));
