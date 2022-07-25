f8b69cabcd333ad8b1d9293d37a9fd78d1d0a07f
João H <joaoheldermartins@hotmail.com>
2022-07-25 18:17:45 -0300

    resolução do exercicio

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/exercicio1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicios/exercicio1.js
@@ -1,60 +1,60 @@
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
+      name: "George R. R. Martin",
       birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
+      name: "Stephen King",
       birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
@@ -64,7 +64,7 @@ const books = [
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 
-//exercicio 1
+//exercicio 1 Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
 function authorBornIn1947(date) {
   const author = books.find((book) => book.author.birthYear === date);
   return `O autor do livro escrito em ${date} é ${author.author.name}!`;
@@ -74,13 +74,13 @@ console.log(authorBornIn1947(1947));
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 
-//exercicio 2
+//exercicio 2 Retorne o nome do livro de menor nome.
 function smallerName() {
-  let nameBook = 'essa é uma string grande para ser comparada'
+  let nameBook = "essa é uma string grande para ser comparada";
 
   books.forEach((book) => {
-    if(book.name.length < nameBook.length){
-      nameBook = book.name
+    if (book.name.length < nameBook.length) {
+      nameBook = book.name;
     }
   });
   // Variável nameBook que receberá o valor do menor nome;
@@ -91,8 +91,43 @@ console.log(`o nome do livro de menor nome é: ${smallerName()}!`);
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////
 
-// exercicio 3
+// exercicio 3 Encontre o primeiro livro cujo nome possui 26 caracteres.
 function getNamedBook() {
   return books.find((book) => book.name.length === 26);
 }
-console.log(getNamedBook())
+console.log(getNamedBook());
+
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// exercicio 4 Ordene os livros por data de lançamento em ordem decrescente.
+function booksOrderedByReleaseYearDesc(arr) {
+  arr.sort((a, b) => (a.releaseYear < b.releaseYear ? 1 : -1));
+  console.log(books);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// exercicio 5 Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
+
+function everyoneWasBornOnSecXX(arr) {
+  return arr.every((a) => a.author.birthYear > 1900 && a.author.birthYear < 2000);
+}
+console.log(everyoneWasBornOnSecXX(books));
+
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// exercicio 6 Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
+
+function someBookWasReleaseOnThe80s(arr) {
+  return arr.some((a) => a.releaseYear > 1980 && a.releaseYear < 1989);
+}
+console.log(someBookWasReleaseOnThe80s(books));
+
+//////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+// exercicio 7 Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
